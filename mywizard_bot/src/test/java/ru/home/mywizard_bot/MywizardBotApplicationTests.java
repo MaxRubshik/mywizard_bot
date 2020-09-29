5daf073dbffed8c322ef47781257b3b475b3988b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-09-29 21:44:13 +0300

    basic functions

---
+++ mywizard_bot/src/test/java/ru/home/mywizard_bot/MywizardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.mywizard_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywizardBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
